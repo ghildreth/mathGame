f68c065f6b50b3d1e39850e7f1c3088ec3d1a216
ghildreth <gabrielantonhildreth@gmail.com>
2018-04-24 02:37:06 +0000

    added in the question generator

---
+++ main.rb
@@ -0,0 +1,19 @@
+class Player
+  attr_reader :lives, :response
+  def initialize()
+    @lives = 3
+    @response =
+  end
+
+
+class Question
+  attr_reader: math_problem
+
+  def initialize(math_problem)
+    @math_problem =
+  end
+end
+
+class Game
+  attr_reader: :player, :question, :score
+
--- question.rb
+++ question.rb
@@ -1,19 +1,52 @@
+class Question
+  attr_reader :num1, :num2
 
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+  end
 
-puts "Welcome to my wonderful Math game! Press 1 to enter!"
-
-print "> "
-enter = $stdin.gets.chomp
-
-if enter =="1"
-  puts "What is the sum of 1 + 2 ?"
+def question_maker
+  puts "What is the sum of #{@num1} + #{@num2} ?"
   print "> "
   answer = $stdin.gets.chomp
 
+  if answer == "#{@num1 + @num2}"
+    puts "Good job! Your wits saved one of your precious lives..."
 
-if answer == "3"
-  puts "Good job!"
-else
-  puts "WROOOOOOOOOOOOONG!"
+  else
+    puts "Wrong, you suck and lost a life."
+  end
+
 end
 end
+
+game1 = Question.new
+game1.question_maker
+
+# puts "Welcome to my wonderful Math game! Press 1 to enter!"
+
+# print "> "
+# enter = $stdin.gets.chomp
+# # if enter =="1"
+# range = (1..20).to_a
+
+
+# def random_number(array)
+#   return array.sample
+# end
+
+# num1 = random_number(range)
+# num2 = random_number(range)
+
+#   puts "What is the sum of #{num1} + #{num2} ?"
+#   print "> "
+#   answer = $stdin.gets.chomp
+
+# if answer == "#{num1} + #{num2}"
+#   puts "Good job!"
+
+# else
+#   puts "WROOOOOOOOOOOOONG!"
+# end
+
