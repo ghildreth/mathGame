e5c552cf303ebc3e2f52df1ace943110acb5992d
ghildreth <gabrielantonhildreth@gmail.com>
2018-04-24 18:25:33 +0000

    finished the game!

--- game.rb
+++ game.rb
@@ -1,14 +1,12 @@
-require_relative 'question'
+# require_relative 'question'
 class Game
   attr_reader :num_of_players
 
   def initialize(num_of_players)
-  # @num_of_players = (1..num_of_players).map { Player.new }
-    random_name1 = ['Gabe', 'Morgan', 'Matt', 'Sadie', "Wendy"].sample
-    random_name2 = ['Gabe', 'Morgan', 'Matt', 'Sadie', "Wendy"].sample
-    # @player1 = Player.new("Player1")
-    # @player2 = Player.new("Player2")
-    # @current_player = @name1
+
+    random_name1 = ['Gabe', 'Matt', 'Sadie', "Wendy"].sample
+    random_name2 = ['Andy', 'Morgan', 'Dominic', 'Natalie'].sample
+
     @player1 = Player.new(random_name1)
     @player2 = Player.new(random_name2)
     @current_player = @player1
@@ -16,35 +14,34 @@ class Game
   end
 
   def new_turn
-    if(@current_player == @player1)
-      @current_player = @player2
-    else
-      @current_player = @player1
-    end
+    @current_player = @current_player == @player1 ? @player2 : @player1
+    # if(@current_player == @player1)
+    #   @current_player = @player2
+    # else
+    #   @current_player = @player1
+    # end
+    puts "<=_.-*'.-*'.-*' NEW TURN '*-.'*-.'*-._=> \n "
   end
 
   def play_game
-    num1 = rand(1..20)
-    num2 = rand(1..20)
+    question = Question.new
+    question.question_maker(@current_player.name)
+    user_answer = $stdin.gets.chomp
 
-    Question.question_maker(num1, num2)
-    answer = $stdin.gets.chomp
-
-    if answer == "#{num1 + num2}"
-      puts "Good job, #{@current_player.name}! Your wits saved one of your precious lives..."
+    if question.verify_answer(user_answer)
+      puts "Good job (^_^), #{@current_player.name}! Your wits saved one of your precious lives... \n "
     else
-      puts "Wrong, #{@current_player.name} you suck and lost a life."
+      puts "Wrong (>.<), #{@current_player.name} you suck and lost a life. \n "
       @current_player.lose_life
-      p @current_player.lives
+      puts "[#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3] \n "
     end
     new_turn
   end
-# start play def
-# then game over
+
   def start_game
     while @player1.is_alive and @player2.is_alive
       play_game
     end
-    puts "Game over #{@current_player.name} wins!"
+    puts "GAME OVER, #{@current_player.name} wins!"
   end
 end
--- question.rb
+++ question.rb
@@ -3,14 +3,18 @@ class Question
   attr_reader :num1, :num2
 
   def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
 
   end
-  def self.question_maker(num1, num2)
-
-    puts "What is the sum of #{num1} + #{num2} ?"
+  def question_maker(current_player)
+    puts "#{current_player} What is the sum of #{@num1} + #{@num2} ?"
     print "> "
   end
 
+  def verify_answer(user_answer)
+    user_answer == "#{@num1 + @num2}"
+  end
 end
 
 # q = Question.new
